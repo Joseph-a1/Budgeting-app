5c769254fdae2ba93e6d7caf083dafdb2e2a7621
Joseph-a1 <erujoseph2006@gmail.com>
2026-04-11 16:54:56 -0700

    connect database

--- db.js
+++ db.js
@@ -1,11 +1,21 @@
-const { Pool } = require("pg");
+const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "Users",
-  password: "paul123",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
+// const { Pool } = require("pg");
+
+// const pool = new Pool({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "Users",
+//   password: "paul123",
+//   port: 5432,
+// });
+
+// module.exports = pool;
